d38960379368a9e33f394bbb2bf3542304ccf8c6
vladmatache224108 <224108@buas.nl>
2023-03-15 09:37:14 +0100

    Update
    Fixed day count

--- LazyFiles/main.py
+++ LazyFiles/main.py
@@ -4,7 +4,8 @@ key = input('Please provide valid API key:\n')
 number_of_days = input('How many days would you like to log?\n')
 links = input('Please provide the links with one comma in between:\n')
 links = links.split(',')
-day = 1
-while day <= int(number_of_days):
+day = 0
+while day < int(number_of_days):
     app.run_logger(key,links[day],day)
     day = day + 1
+print('Done!')
